7f78e03c73ef04afd2e7567377b7e489949e4127
RodrigoNavares <rodrigo.navares@zener.es>
2018-08-29 14:38:31 +0200

    inicio de sesion y navbar

--- educando/urls.py
+++ educando/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^nuevo_usuario/', views.nuevo_usuario, name='nuevo_usuario'),
     url(r'^ingresar/$',views.ingresar, name='ingresar'),
     url(r'^privado_admin/$',views.privado_admin, name='privado_admin'),
+    url(r'^privado_alumno/$',views.privado_alumno, name='privado_alumno'),
+    url(r'^privado_profesor/$',views.privado_profesor, name='privado_profesor'),
 ]
--- educando/views.py
+++ educando/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from educando.forms import *
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 def nuevo_usuario(request):
     if request.method=='POST':
@@ -12,13 +14,11 @@ def nuevo_usuario(request):
         if formulario.is_valid():
             formulario.save()
             return HttpResponseRedirect('/usuarios/privado_admin/')
-    else:
-        formulario = UserCreationForm()
-    return render(request,'educando/usuario.html', {'formulario':formulario})
 
 def ingresar(request):
+    formulario = AuthForm()
     if request.method == 'POST':
-        formulario = AuthForm(request.POST)
+        formulario = AuthForm(data=request.POST)
         if formulario.is_valid():
             usuario = request.POST['username']
             clave = request.POST['password']
@@ -26,14 +26,37 @@ def ingresar(request):
             if acceso is not None:
                 if acceso.is_active:
                     login(request, acceso)
-                    return HttpResponseRedirect('/usuarios/privado_admin')
+                    perfil = Usuarios_Perfiles.objects.filter(id_usuario_id=acceso.id).values('id_perfil__perfil')
+                    if len(perfil)>0:
+                        if perfil[0]['id_perfil__perfil']== 'ADMINISTRADOR':
+                            return HttpResponseRedirect('/usuarios/privado_admin')
+                        elif perfil[0]['id_perfil__perfil']== 'PROFESOR':
+                            return HttpResponseRedirect('/usuarios/privado_profesor')
+                        elif perfil[0]['id_perfil__perfil']== 'ALUMNO':
+                            return HttpResponseRedirect('/usuarios/privado_alumno')
+                    else:
+                        return render(request, 'educando/nopermisos.html')
                 else:
                     return render(request, 'educando/noactivo.html')
-            else: return render(request, 'educando/nousuario.html')
-    else: formulario = AuthForm()
+            else:
+                return render(request, 'educando/nousuario.html')
+        else:
+            messages.add_message(request, messages.ERROR, 'Usuario y contraseña no coinciden')
+            return render(request, 'educando/ingresar.html', {'formulario': formulario})
     return render(request, 'educando/ingresar.html', {'formulario':formulario})
 
 @login_required(login_url='educando/ingresar')
 def privado_admin(request):
+    formulario = UserCreationForm()
     usuario= request.user
-    return render(request, 'educando/privado_admin.html', {'usuario':usuario})
+    return render(request, 'educando/privado_admin.html', {'usuario':usuario, 'formulario':formulario})
+
+@login_required(login_url='educando/ingresar')
+def privado_alumno(request):
+    usuario= request.user
+    return render(request, 'educando/privado_alumno.html', {'usuario':usuario})
+
+@login_required(login_url='educando/ingresar')
+def privado_profesor(request):
+    usuario= request.user
+    return render(request, 'educando/privado_profesor.html', {'usuario':usuario})
